0841dbd30a4637e7e5c6d36d4b125cfc16e55d5a
Andy Pattenden <andy.pattenden@specialistholidays.com>
2017-06-15 16:24:49 +0100

    Managing and Updating Component State
    https://online.reacttraining.com/courses/reactjsfundamentals/lectures/2466734

--- app/index.js
+++ app/index.js
@@ -1,34 +1,9 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var PropTypes = require('prop-types');
 require('./index.css');
-
-class Badge extends React.Component {
-	render() {
-		return (
-			<div>
-				<img
-					src={this.props.img}
-					alt='Avatar'
-					style={{width: 100, height: 100}}
-				/>
-				<h1>Name: {this.props.name}</h1>
-				<h3>Username: {this.props.username}</h3>
-			</div>
-		)
-	}
-}
-
-Badge.propTypes = {
-	img: PropTypes.string.isRequired,
-	name: PropTypes.string.isRequired,
-	username: PropTypes.string.isRequired
-}
+var App = require('./components/App');
 
 ReactDOM.render(
-	<Badge
-		name="Andy Pattenden"
-		username="andypattenden"
-		img='https://avatars0.githubusercontent.com/u/6861048?v=3&s=460' />
+	<App />
 	, document.getElementById('app')
 );
