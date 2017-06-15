ec0407eaafc93ed2ff3fe4721d71aefaa43195d1
Andy Pattenden <andy.pattenden@specialistholidays.com>
2017-06-15 15:35:41 +0100

    Prop Types
    https://online.reacttraining.com/courses/reactjsfundamentals/lectures/2466636

--- app/index.js
+++ app/index.js
@@ -1,18 +1,34 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var PropTypes = require('prop-types');
 require('./index.css');
 
-class App extends React.Component {
+class Badge extends React.Component {
 	render() {
 		return (
 			<div>
-				Hello React Training!
+				<img
+					src={this.props.img}
+					alt='Avatar'
+					style={{width: 100, height: 100}}
+				/>
+				<h1>Name: {this.props.name}</h1>
+				<h3>Username: {this.props.username}</h3>
 			</div>
 		)
 	}
 }
 
+Badge.propTypes = {
+	img: PropTypes.string.isRequired,
+	name: PropTypes.string.isRequired,
+	username: PropTypes.string.isRequired
+}
+
 ReactDOM.render(
-	<App />
+	<Badge
+		name="Andy Pattenden"
+		username="andypattenden"
+		img='https://avatars0.githubusercontent.com/u/6861048?v=3&s=460' />
 	, document.getElementById('app')
 );
