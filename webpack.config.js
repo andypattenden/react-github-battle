db7a06b6ca57ae3b6308aacbbf115a066ad8ee0a
Andy Pattenden <andy.pattenden@specialistholidays.com>
2017-06-19 20:17:08 +0100

    React Router V4
    https://online.reacttraining.com/courses/reactjsfundamentals/lectures/2466777

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
 	, output: {
 		path: path.resolve(__dirname, 'dist')
 		, filename: 'index_bundle.js'
+		, publicPath: '/'
 	}
 	, module: {
 		rules: [
@@ -17,6 +18,9 @@ module.exports = {
 				, use: ['style-loader', 'css-loader']
 			}
 		]
+	}
+	, devServer: {
+		historyApiFallback: true
 	}
 	, plugins: [
 		new HtmlWebpackPlugin({
